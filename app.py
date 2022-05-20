b622079be8f9eaad38ae7d29fb2d4b7b7e6f394d
nheckman29 <nheckman29@gmail.com>
2022-05-20 17:01:13 -0400

    Initial Menu Bar

--- app.py
+++ app.py
@@ -12,13 +12,40 @@ app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def initial():
+    return redirect("/home")
+
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
     """
     Initial Site Landing Page
     """
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/resume", methods=["GET", "POST"])
+def resume():
+    """
+    Resume Preview and Download
+    """
+    return render_template("resume.html")
+
+
+@app.route("/projects", methods=["GET", "POST"])
+def projects():
+    """
+    Project Descriptions and Demos/Pictures
+    """
+    return render_template("projects.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """
+    Personal Contact and Email Functionality
+    """
+    return render_template("contact.html")
 
 
 app.run(
